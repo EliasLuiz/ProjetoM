e5ec833425c8ed4ee1887ff1475552d5c399214e
EliasLuiz <eliasluizjr@gmail.com>
2014-11-09 11:00:34 -0200

    database.inep2012 OK OK

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ from database.inep2012 import dados_aluno as aluno
 from database.inep2012 import dados_docente as docente
 import sys
 
-#diretorio = raw_input("Pasta da base de dados: ")
+diretorio = raw_input("Pasta da base de dados: ")
 
-ies.txt2db('/media/elias/4DF85F7166976ABF/CEFET/Projeto Maurilio/microdados_educacao_superior_2012')
-#localOferta.txt2db('/media/elias/4DF85F7166976ABF/CEFET/Projeto Maurilio/microdados_educacao_superior_2012')
-#curso.txt2db('/media/elias/4DF85F7166976ABF/CEFET/Projeto Maurilio/microdados_educacao_superior_2012')
-#docente.txt2db('/media/elias/4DF85F7166976ABF/CEFET/Projeto Maurilio/microdados_educacao_superior_2012')
-#aluno.txt2db('/media/elias/4DF85F7166976ABF/CEFET/Projeto Maurilio/microdados_educacao_superior_2012')
+ies.txt2db(diretorio)
+localOferta.txt2db(diretorio)
+curso.txt2db(diretorio)
+docente.txt2db(diretorio)
+aluno.txt2db(diretorio)
 
 sys.exit(0)
